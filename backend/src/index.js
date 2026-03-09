1f92af0647191f751f296678926b7d3b824fac55
Antonio <delatorreperezantonio@gmail.com>
2026-03-10 00:18:39 +0100

    Fix greeting message in root endpoint response

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const fastify = require("fastify")({
 });
 
 fastify.get("/", async (request, reply) => {
-  return { hello: "pepe" };
+  return { hello: "mundo" };
 });
 
 const start = async () => {
